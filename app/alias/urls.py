772513ef62a66b5b3a0bd79f1b0c85e8ddb6a50b
DanilJr <danilu4444@gmail.com>
2021-02-17 20:26:55 +0200

    setted up some functionality

--- app/alias/urls.py
+++ app/alias/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('book/<slug:book_slug>', get_by_slug, name='get_by_slug'),
 ]
--- app/alias/views.py
+++ app/alias/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('all is ok')
+
+
+def get_by_slug(request):
+    pass
+    #return render()
